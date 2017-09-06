1060c4306d8324f0d2308336d19d0de3580656d5
Andy Hawkins <andy@gently.org.uk>
2017-09-06 10:24:37 +0100

    Cron script need explicit path to root's home directory

--- borg-backup-cron.sh
+++ borg-backup-cron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-. ~/.borgbackup/borg-settings.sh
+. /root/.borgbackup/borg-settings.sh
 
 DIR="$(dirname "$(test -L "$0" && readlink -f "$0" || echo "$0")")"
 $DIR/borg-backup.sh 2>&1 | Mail -s "`hostname -s` Borg backup report" $MAILNAME
