560767f06f7236d2d19e992af816d9a0360f48c2
Andy Hawkins <andy@gently.org.uk>
2018-01-09 10:35:42 +0000

    Remove old comments

--- borg-backup.sh
+++ borg-backup.sh
@@ -11,8 +11,6 @@ then
 	EXCLUDE="--exclude-from /root/.borgbackup/excludes"
 fi
 
-# Backup all of /home and /var/www except a few
-# excluded directories
 borg create --remote-path borg1 -v --stats --exclude-caches \
 		$EXCLUDE \
     $BORG_REPO::'{now:%Y-%m-%d-%H-%M-%S}' \
@@ -22,10 +20,6 @@ echo
 echo "Pruning..."
 echo
 
-# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
-# archives of THIS machine. The '{hostname}-' prefix is very important to
-# limit prune's operation to this machine's archives and not apply to
-# other machine's archives also.
 borg prune --remote-path borg1 -v --list $BORG_REPO \
     --keep-hourly=$HOURLY --keep-daily=$DAILY --keep-weekly=$WEEKLY --keep-monthly=$MONTHLY
 
