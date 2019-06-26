b7528bdd4ec8f480ff55e98f25050f6a64425b24
Andy Hawkins <andy@gently.org.uk>
2019-06-26 13:25:35 +0100

    Retry borg create and prune when backing up

--- borg-backup.sh
+++ borg-backup.sh
@@ -17,17 +17,23 @@ then
 	EXCLUDE="--exclude-from /root/.borgbackup/excludes"
 fi
 
-/usr/local/sbin/borg create -v --stats --exclude-caches \
+if ! retry 5 5 /usr/local/sbin/borg create -v --stats --exclude-caches \
 		$EXCLUDE \
     $BORG_REPO::'{now:%Y-%m-%d-%H-%M-%S}' \
 		"${DIRECTORIES[@]}" # expand the array, quoting each element
+then
+	echo "Backup failed"
+fi
 
 echo
 echo "Pruning..."
 echo
 
-/usr/local/sbin/borg prune -v --list $BORG_REPO \
+if ! retry 5 5 /usr/local/sbin/borg prune -v --list $BORG_REPO \
     --keep-hourly=$HOURLY --keep-daily=$DAILY --keep-weekly=$WEEKLY --keep-monthly=$MONTHLY
+then
+	echo "Prune failed"
+fi
 
 echo
 echo "Complete - quota..."
