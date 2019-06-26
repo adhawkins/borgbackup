7d96beffd4442bbdd9e10b93fa87ee36d348721c
Andy Hawkins <andy@gently.org.uk>
2019-06-26 13:25:16 +0100

    Add function to retry borg commands

--- borg-complete-settings.sh
+++ borg-complete-settings.sh
@@ -1,3 +1,41 @@
+retry()
+{
+	success=1
+
+	if [ "$#" -ge "4" ]
+	then
+		retries=$1
+		delay=$2
+		command=$3
+
+		shift 3
+
+		tries=0
+
+		while [ "$tries" -lt "$retries" ]
+		do
+			if $command "$@"
+			then
+				success=0
+				break
+			else
+				tries=$((tries+1))
+				echo "Failed - $tries attempt(s)"
+
+				if [ "$tries" -lt "$retries" ]
+				then
+					sleep $delay
+				fi
+			fi
+
+		done
+	else
+		echo "Usage: func retries delay command args"
+	fi
+
+	return $success
+}
+
 if [ -n "${BORGBASE_REPO_ID}" ]
 then
 	export SSH_ROOT="${BORGBASE_REPO_ID}@${BORGBASE_REPO_ID}.repo.borgbase.com"
