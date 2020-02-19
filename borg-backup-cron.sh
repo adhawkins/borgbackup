2e88809e560575e54ff4af7c71aad7faf159e5e4
Andy Hawkins <andy@gently.org.uk>
2020-02-19 16:33:30 +0000

    Add random sleep before starting cron backup

--- borg-backup-cron.sh
+++ borg-backup-cron.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SLEEP=$(( RANDOM % 900 ))
+echo "Sleeping  for $SLEEP seconds"
+sleep $SLEEP
+
 . /root/.borgbackup/borg-settings.sh
 
 DIR="$(dirname "$(test -L "$0" && readlink -f "$0" || echo "$0")")"
