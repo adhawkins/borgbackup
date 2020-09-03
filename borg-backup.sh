9cec2187fd65bb1635c9d453f208629dc4564bf1
Andy Hawkins <andy@gently.org.uk>
2020-09-03 11:58:54 +0100

    Run borg check every 10 days

--- borg-backup.sh
+++ borg-backup.sh
@@ -47,7 +47,7 @@ fi
 if ! retry 5 5 /usr/local/sbin/borg create -v --show-rc --stats --exclude-caches \
 		--remote-ratelimit $RATELIMIT \
 		$EXCLUDE \
-    $BORG_REPO::'{now:%Y-%m-%d-%H-%M-%S}' \
+		$BORG_REPO::'{now:%Y-%m-%d-%H-%M-%S}' \
 		"${DIRECTORIES[@]}" # expand the array, quoting each element
 then
 	echo "Backup failed"
@@ -58,7 +58,7 @@ echo "Pruning..."
 echo
 
 if ! retry 5 5 /usr/local/sbin/borg prune -v --show-rc --list $BORG_REPO \
-    --keep-hourly=$HOURLY --keep-daily=$DAILY --keep-weekly=$WEEKLY --keep-monthly=$MONTHLY
+		--keep-hourly=$HOURLY --keep-daily=$DAILY --keep-weekly=$WEEKLY --keep-monthly=$MONTHLY
 then
 	echo "Prune failed"
 fi
@@ -72,4 +72,30 @@ then
 	echo "Using borgbase, quota currently unavailable"
 else
 	ssh $SSH_ROOT quota
+fi
+
+STAMPFILE="${HOME}/borg-check.timestamp"
+
+if [ -e "$STAMPFILE" ]
+then
+		if [ $(find "$STAMPFILE" -mtime +10) ]
+		then
+				echo
+				echo "Checking"
+				echo
+				${DIR}/borg-check.sh
+				if [ $? = 0 ]
+				then
+						echo "Repository check successful"
+						touch "$STAMPFILE"
+				else
+						echo "*** Repository check failed ***"
+				fi
+		fi
+else
+		touch -d @"$(echo "$(date +%s) - $(shuf -i1-10 -n1) * 86400" | bc)" "$STAMPFILE"
+
+		echo
+		echo "Check stamp initialized"
+		echo
 fi
